c531ec28392aafebc2d126bd59bb7d78ea6c313b
wass01 <maatallawassim01@gmail.com>
2021-04-12 16:47:40 +0200

    milestone 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,21 @@ let app = new Vue({
   data: {
     searchMovie: '',
     movies: [],
+    series: [],
   },
 
   methods: {
     search: function() {
+      // chiamata film
       axios.get(`https://api.themoviedb.org/3/search/movie?api_key=390be9a6b25f187e23b42fdb13f8ed80&query=${this.searchMovie}&language=it`)
-      .then((response) => {
-        this.movies = response.data.results;
+      .then((movie) => {
+        this.movies = movie.data.results;
+      });
+
+      // chiamata serie tv
+      axios.get(`https://api.themoviedb.org/3/search/tv?api_key=390be9a6b25f187e23b42fdb13f8ed80&query=${this.searchMovie}&language=it`)
+      .then((serie) => {
+        this.series = serie.data.results;
       });
     }
   },

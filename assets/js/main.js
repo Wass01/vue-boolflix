856f7690d0ce9d0a2e913d8634945843d5e7fa9c
wass01 <maatallawassim01@gmail.com>
2021-04-12 15:58:03 +0200

    milestone 1 quasi finita

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,16 @@ Vue.config.devtools = true;
 let app = new Vue({
   el: "#root",
   data: {
+    searchMovie: '',
+    movies: [],
   },
-  
+
   methods: {
+    search: function() {
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=390be9a6b25f187e23b42fdb13f8ed80&query=${this.searchMovie}&language=it`)
+      .then((response) => {
+        this.movies = response.data.results;
+      });
+    }
   },
 });
